2713dac08cf34a812b142364a14f556933b04011
chege54 <chege54@gmail.com>
2024-07-14 16:07:54 +0200

    Add cost getters

--- cost.py
+++ cost.py
@@ -9,7 +9,10 @@ class FixCost:
     '''
     def __init__(self, name, x:Q_) -> None:
         self.name = name
-        self.value = x
+        self.cost = x
+
+    def get_cost(self) -> Q_:
+        return self.cost
 
 
 class ProportionalCost:
@@ -19,12 +22,14 @@ class ProportionalCost:
     def __init__(self, name, cost_per_1:Q_, x:Q_) -> None:
         self.name = name
         self.cost_per_x = cost_per_1
-        self.value = cost_per_1 * x
+        self.cost = (cost_per_1 * x).to_base_units()
 
+    def get_cost(self) -> Q_:
+        return self.cost
 
 class ManualCost(ProportionalCost):
     '''
-
+    Handmade costs
     '''
     def __init__(self, name: Q_, x: Q_) -> None:
         super().__init__(name=name, cost_per_1=CostParameters.manual_cost_per_hour, x=x)
@@ -34,13 +39,20 @@ class ManufacturingCost:
     '''
         Class that represents a workhorse job.
     '''
-    def __init__(self, name, material_name:str, machine_name:str, time:Q_, material_weight:Q_) -> None:
+    def __init__(self, name, machine_name:str, material_name:str, material_weight:Q_, time:Q_, ) -> None:
         self.name = name
-        self.material = Materials[material_name]
         self.machine = Machines[machine_name]
+        self.material = Materials[material_name]
+        self.time = time
+        self.material_weight = material_weight
 
-        electricity_cost_per_time = CostParameters.price_per_kwh / self.machine.enery_consumption
-        self.electricity_cost = ProportionalCost("Electricity", cost_per_1=electricity_cost_per_time, x=time)
-        self.material_cost = self.material.price_per_weight * material_weight
+        # used for debugging
+        self.electricity_cost = self.machine.get_electricity_cost(self.time)
+        self.material_cost = self.material.get_cost(self.material_weight)
+        self.amortization_cost = self.machine.get_amortization_cost(self.time)
 
-        self.value = self.electricity_cost + self.material_cost +  self.machine.amortization_cost
+    def get_cost(self):
+        total = self.electricity_cost + \
+            self.material_cost + \
+            self.amortization_cost
+        return total
--- material_db.py
+++ material_db.py
@@ -7,7 +7,7 @@ class Material:
     def __init__(self, name,
                  price:Q_,
                  diameter:Q_,
-                 spool_weight:Q_,
+                 total_weight:Q_,
                  density:Q_ = None,
                  length:Q_ = None) -> None:
         self.name = name
@@ -17,17 +17,21 @@ class Material:
             self.density = density
             # l = weight / (density * cross_section_area_cm2)
             cross_section_area = (diameter * diameter) * 3.1415 / 4 # area
-            calculated_length = (spool_weight / (density * cross_section_area)) # convert to mm
+            calculated_length = (total_weight / (density * cross_section_area)) # convert to mm
             print(f"{calculated_length=}" ) #TODO: add logging
             self.length = calculated_length
 
-        self.price_per_weight = price / spool_weight
+        self.price_per_weight = price / total_weight
         self.price_per_length = price / self.length if self.length else None
+
+    def get_cost(self, weight:Q_) -> Q_:
+        material_cost = (self.price_per_weight * weight).to_base_units()
+        return material_cost
 
 #### Material database ####
 Materials = {
     "PLA" : Material(name="3DJAKE ecoPLA",
                      price =        8650 * ureg.dimensionless,
                      diameter =     1.75 * ureg.millimeter,
-                     spool_weight = 1 * ureg.kilogram)
+                     total_weight = 1 * ureg.kilogram)
 }
