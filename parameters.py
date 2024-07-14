77c0514df1273244384e51a262a5f14e16f3116b
chege54 <chege54@gmail.com>
2024-07-14 16:06:56 +0200

    Update parameter dimensions

--- parameters.py
+++ parameters.py
@@ -2,6 +2,6 @@ from pint_units import ureg
 
 class CostParameters: # TODO: move to config file
     currency = "huf" # TODO: currently it is "dimensionless"
-    price_per_kwh        = 40 * ureg.dimensionless / (ureg.kilowatt*ureg.hour)  # in price per kWh
-    manual_cost_per_hour = 10000 * ureg.dimensionless / ureg.hour # in huf per hour
+    price_per_kwh        = 40 * (ureg.dimensionless / ureg.kilowatthour) # in price per kWh
+    manual_cost_per_hour = 10000 * (ureg.dimensionless / ureg.hour) # in huf per hour
     safety_factor        = 5 * ureg.percent # in percent
